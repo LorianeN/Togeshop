c7b5f454a84a656196843cb4481a0e4b9e60f686
Sandjiv Parassouramanaick <sandjiv.parassou@gmail.com>
2015-11-20 00:38:38 +0100

    creation de tout le flw de l'application (activités plus liens qui vont biens) reste à compléter les actiivtés maintenant

--- app/src/main/java/com/example/loriane/togeshop/LoginActivity.java
+++ app/src/main/java/com/example/loriane/togeshop/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.loriane.togeshop;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -51,6 +52,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
+    private static final String EXTRA_MSG = "Roger" ;
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -333,7 +335,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent intent = new Intent(LoginActivity.this,
+                        ChoixListe.class);
+                intent.putExtra("nom",EXTRA_MSG);
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
