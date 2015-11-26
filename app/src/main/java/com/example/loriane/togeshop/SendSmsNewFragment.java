e612c6c23af0029410d208134e86d932b06657b4
Sandjiv Parassouramanaick <sandjiv.parassou@gmail.com>
2015-11-26 17:18:56 +0100

    ya de l'avancée dans la complétion des numéros

--- app/src/main/java/com/example/loriane/togeshop/SendSmsNewFragment.java
+++ app/src/main/java/com/example/loriane/togeshop/SendSmsNewFragment.java
@@ -1,12 +1,20 @@
 package com.example.loriane.togeshop;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -54,17 +62,16 @@ public class SendSmsNewFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_sms_new, container, false);
+        View view= inflater.inflate(R.layout.fragment_send_sms_new, container, false);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -73,6 +80,11 @@ public class SendSmsNewFragment extends Fragment {
             mListener.onFragmentInteraction(uri);
         }
     }
+
+
+
+
+
 
     @Override
     public void onAttach(Activity activity) {
@@ -88,21 +100,6 @@ public class SendSmsNewFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
 }
